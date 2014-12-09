775766a4cc51e1986059c2a5d95b5ed5782e7661
adamm <adamm24680@gmail.com>
2014-12-09 19:24:32 +0100

    init

---
+++ ZPP - frontend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZPP___frontend.MvcApplication" Language="C#" %>
---
+++ ZPP - frontend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZPP___frontend.Startup))]
+namespace ZPP___frontend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
